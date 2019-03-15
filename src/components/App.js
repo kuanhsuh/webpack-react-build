215fb723d364515e6d812366ddbacf652094630a
dannyhuang <kuanhsuh@gmail.com>
2019-03-15 11:42:03 +0800

    Git Init

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import css from './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>My React Webpack Build Tutorial 😍</h1>
+        <p>I love React</p>
+      </div>
+    )
+  }
+}
+
+export default App
